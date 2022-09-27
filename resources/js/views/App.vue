eb05ffa2a6da55acb0a12c7dc371165a13e31281
bittoMazza <robertomazza99@gmail.com>
2022-09-27 15:06:36 +0200

    created axios call function

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -7,11 +7,35 @@
 </template>
 
 <script>
+import Axios from 'axios';
+
 export default {
     name: "App",
+    data:function(){
+        return{
+            posts : [],
+        }
+    },
     components:{
 
+    },
+    methods:{
+        getPosts(postsPage = 1){
+            Axios.get('/api/posts',{
+                page: postsPage
+            }).then((response) => {
+                console.log(response.data.results);
+                this.posts = response.data.results.data;
+            }).catch((error) => {
+                console.error(error);
+            })
+        }
+    },
+    created(){
+        this.getPosts();
     }
+
+
 }
 </script>
 
