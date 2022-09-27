8d07dfdc3a195933fe15058dc15f868d26385eb1
bittoMazza <robertomazza99@gmail.com>
2022-09-27 14:42:53 +0200

    create front.js file and add to webpack.mix

---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+        <h1>
+            Ciao sono un titolone
+        </h1>
+   </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
